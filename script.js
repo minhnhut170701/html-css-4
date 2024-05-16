631b57f772b6ab2113c35caf762786beae3d3d3f
minhnhut170701 <minhnhut170701@gmail.com>
2024-05-16 14:17:45 +0700

    fix style

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
     this.classList.toggle('show');
+    const body  = document.querySelector('body')
+    body.classList.toggle('overFlow')
     const menu = document.querySelector('.menu-mb');
     menu.classList.toggle('hide-menu');
 });
